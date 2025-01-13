fc82ed65e689071d0c34e8f6f79638f33047e4e9
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-01-13 00:55:04 -0800

    Fix broken setup

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ fn hello_from_rust() -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn ecs_165a_database(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn lstore(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hello_from_rust, m)?)?;
     Ok(())
 }
