51f67565103c18b467b31c9064850085eded43f8
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-01-28 10:56:28 -0800

    Add Database class in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 use pyo3::prelude::*;
 
+#[pyclass]
+struct Database {
+    #[pyo3(get, set)]
+    page_size: usize,
+}
+
+#[pymethods]
+impl Database {
+    #[staticmethod]
+    fn ping() -> String {
+        return String::from("pong!");
+    }
+}
+
 /// Blazingly fast hello
 #[pyfunction]
 fn hello_from_rust() -> PyResult<String> {
@@ -9,6 +23,7 @@ fn hello_from_rust() -> PyResult<String> {
 /// A Python module implemented in Rust.
 #[pymodule]
 fn lstore(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<Database>()?;
     m.add_function(wrap_pyfunction!(hello_from_rust, m)?)?;
     Ok(())
 }
