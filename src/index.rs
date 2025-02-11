d57b33e5ac30f62ba14c37f1df570ee035e3f9c3
Keanu Chau <kechau@ucdavis.edu>
2025-02-11 10:54:03 -0800

    Add index class methods

--- src/index.rs
+++ src/index.rs
@@ -9,21 +9,16 @@ use crate::database::{Database, Table, Columns, Column};
 #[pyclass]
 pub struct Index {
     /*  EXPLANATION OF BTree, NOT TOO SURE ABOUT THIS.
-        Option<BTreeMap<...>>: Each element in the vector can be either:
-        -- None (no index exists for the column), or --
-        -- ome(BTreeMap<...>) (an index exists). --
+
+        A vector of BTreeMaps, can be either Some::BTreeMap or None as its elements.
 
         BTreeMap<i64, Vec<[usize; 3]>>:
         -- BTreeMap: A balanced binary search tree (B-Tree), for maintaining sorted key-value pairs. --
-        -- i64: The key type of the map (e.g., the column value being indexed). --
+        -- i64: The key type of the map (the column value being indexed). --
 
-        Vec<[usize; 3]>: The value type of the map, representing a list of pointers to records.
-         -- Each pointer is represented as a 3-element array of usize, referring to: 
-            -- Page range index --
-            -- Page index within that range --
-            -- Record offset within the page --
+        Vec<[usize; 3]>: Will later change it to the RID of the record.
      */
-    indices: Vec<Option<BTreeMap<i64, Vec<[usize; 3]>>>>, // One BTree per column
+    indices: Vec<Option<BTreeMap<i64, Vec<[usize; 3]>>>>, // change <[usize; 3]> to RID
     table: Table,
 }
 
@@ -31,27 +26,51 @@ impl Index {
     // Init
     // Mandatory: One index for each table. All our empty initially.
     pub fn new(table: Table) -> Index {
-        let mut indices = vec![None; table.columns.len]; // init empty indices
+        let mut indices = vec![None; table.columns.len]; // change to table.num_columns
+        indices[table.key] = Some(BTreeMap::new());
         Index { 
             indices,
             table,
         }
     }
-    // returns the location of all records with the given value on column "column"
-    pub fn locate(&self) {
-
+    /// Returns the location of all records with the given value on column "column"
+    pub fn locate(&self, column: usize, value: i64) -> Option<&Vec<[usize; 3]>> { // change <[usize; 3]> to RID
+        if let Some(tree) = &self.indices[column] {
+            return tree.get(&value);
+        }
+        None
     }
-    // Returns the RIDs of all records with values in column "column" between "begin" and "end"
-    pub fn locate_range(&self) {
-    
+    /// Returns the RIDs of all records with values in column "column" between "begin" and "end"
+    pub fn locate_range(&self, begin: i64, end: i64, column: usize) -> Vec<[usize; 3]> { // change <[usize; 3]> to RID
+        if let Some(tree) = &self.indices[column] {
+                return tree.range(begin..=end).flat_map(|(_, pointers)| pointers.clone()).collect();
+            }
+        }
+        vec![];
     }
-    // optional: Create index on specific column
-    pub fn create_index(&mut self) {
+    // Optional: Create index on specific column
+    pub fn create_index(&mut self, column_num: usize) {
+        // Create BTree for column
+        if self.indices[column_num] == None {
+            self.indices[column_num] = Some(BTreeMap::new());
+            // Populate new index with existing records
 
+        }
+        
     }
-    // optional: Drop index of specific column
-    pub fn drop_index(&mut self) {
-
+    // Insert or update index for a specific column
+    pub fn update_index(&mut self, key: i64, pointer: [usize; 3], column_number: usize) {
+        // Gets column Some::BTreeMap, creates one if None
+        let tree = self.indices[column_number].get_or_insert_with(BTreeMap::new);
+        // Insert or update key
+        // Searches for the given key in the BTree, If the key exists, it returns a mutable reference to the corresponding value, 
+        // If the key does not exist, it creates a new entry in the BTree for the key, If the key does not exist, this initializes an empty vector (Vec::new) as the value for the key.
+        // Appends the provided pointer ([usize; 3]) to the vector associated with the key
+        tree.entry(key).or_insert_with(Vec::new).push(pointer);
+    }
+    // Optional: Drop index of specific column
+    pub fn drop_index(&mut self, column_num: usize) {
+        self.indices[column_num] = None;
     }
 
 }
