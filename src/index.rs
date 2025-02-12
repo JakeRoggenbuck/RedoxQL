e1fc28cf2cb00edd920d199c049aae68d04735a2
Keanu Chau <kechau@ucdavis.edu>
2025-02-11 20:01:27 -0800

    Add delete Index method

--- src/index.rs
+++ src/index.rs
@@ -19,7 +19,7 @@ pub struct Index {
         Vec<[usize; 3]>: Will later change it to the RID of the record.
      */
     indices: Vec<Option<BTreeMap<i64, Vec<[usize; 3]>>>>, // change <[usize; 3]> to RID
-    table: Table,
+    table: RTable,
 }
 
 impl Index {
@@ -33,6 +33,7 @@ impl Index {
             table,
         }
     }
+
     /// Returns the location of all records with the given value on column "column"
     pub fn locate(&self, column: usize, value: i64) -> Option<&Vec<[usize; 3]>> { // change <[usize; 3]> to RID
         if let Some(tree) = &self.indices[column] {
@@ -40,6 +41,7 @@ impl Index {
         }
         None
     }
+
     /// Returns the RIDs of all records with values in column "column" between "begin" and "end"
     pub fn locate_range(&self, begin: i64, end: i64, column: usize) -> Vec<[usize; 3]> { // change <[usize; 3]> to RID
         if let Some(tree) = &self.indices[column] {
@@ -53,27 +55,58 @@ impl Index {
         }
         Vec::new()
     }
+
     /// Create index on specific column
-    pub fn create_index(&mut self, column_num: usize) {
+    pub fn create_index(&mut self, column: usize) {
         // Create BTree for column
-        if self.indices[column_num] == None {
-            self.indices[column_num] = Some(BTreeMap::new());
-            // TODO: Populate new index with existing records
+        if self.indices[column].is_none() {
+            self.indices[column] = Some(BTreeMap::new());
+            // Populate new index with existing records
+
+            // let table = self.table.lock().unwrap();
+            // for rid in table.page_directory.keys() {
+            //     let row = table.fetch_row(*rid);
+            //     let value = row[column];
+
+            //     // Add RID to index
+            //     self.update_index(value, [*rid as usize, column, 0], column).unwrap();
+            // }
             }
     }
+
     /// Insert or update index for a specific column
-    pub fn update_index(&mut self, key: i64, pointer: [usize; 3], column_number: usize) { // change pointer to RID
+    pub fn update_index(&mut self, key: i64, pointer: [usize; 3], column: usize) -> Result<(), String> { // change <[usize; 3]> to RID
+        if column >= self.indices.len() {
+            return Err(format!("Column {} does not exist in table '{}'", column, self.table.name));
+        }
         // Gets column Some::BTreeMap, creates one if None
-        let tree = self.indices[column_number].get_or_insert_with(BTreeMap::new);
+        let tree = self.indices[column].get_or_insert_with(BTreeMap::new);
         // Insert or update key
         // Searches for the given key in the BTree, If the key exists, it returns a mutable reference to the corresponding value, 
         // If the key does not exist, it creates a new entry in the BTree for the key, If the key does not exist, this initializes an empty vector (Vec::new) as the value for the key.
-        // Appends the provided pointer ([usize; 3]) to the vector associated with the key
+        // Appends the provided RID to the vector associated with the key
         tree.entry(key).or_insert_with(Vec::new).push(pointer);
+        Ok(())
     }
+
+    pub fn delete_from_index(&mut self, column: usize, key: i64, pointer: [usize; 3]) { // change <[usize; 3]> to RID
+        if let Some(tree) = &mut self.indices[column] {
+            if let Some(pointers) = tree.get_mut(&key) {
+                // Find the position of the RID to remove
+                if let Some(pos) = pointers.iter().position(|&p| p == pointer) {
+                    pointers.remove(pos);
+                }
+                // If no more RID's exist for this key, remove the key
+                if pointers.is_empty() {
+                    tree.remove(&key);
+                }
+            }
+        }
+    }
+
     /// Drop index of specific column
-    pub fn drop_index(&mut self, column_num: usize) {
-        self.indices[column_num] = None;
+    pub fn drop_index(&mut self, column: usize) {
+        self.indices[column] = None;
     }
 }
 
