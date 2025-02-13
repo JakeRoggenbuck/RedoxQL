7da5fcdf7ea183147c7ed930c30fbc9ce8346167
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-02-12 18:12:58 -0800

    Add tests for the update PR

--- src/query.rs
+++ src/query.rs
@@ -1,4 +1,4 @@
-use super::database::{RTable, Record};
+use super::database::{RDatabase, RTable, Record};
 use pyo3::prelude::*;
 
 #[pyclass]
@@ -46,8 +46,10 @@ impl RQuery {
         };
 
         let base_rid = result[self.table.page_range.base_container.rid_column as usize];
-        let base_schema_encoding = result[self.table.page_range.base_container.schema_encoding_column as usize];
-        let base_indirection_column = result[self.table.page_range.base_container.indirection_column as usize];
+        let base_schema_encoding =
+            result[self.table.page_range.base_container.schema_encoding_column as usize];
+        let base_indirection_column =
+            result[self.table.page_range.base_container.indirection_column as usize];
 
         // base record addresses
         let addrs_base = record.addresses.lock().unwrap();
@@ -55,34 +57,51 @@ impl RQuery {
         if base_rid == base_indirection_column {
             // first update
             if base_schema_encoding == 0 {
-                let mut base_schema_encoding = addrs_base[self.table.page_range.base_container.schema_encoding_column as usize].page.lock().unwrap();
+                let mut base_schema_encoding = addrs_base
+                    [self.table.page_range.base_container.schema_encoding_column as usize]
+                    .page
+                    .lock()
+                    .unwrap();
                 base_schema_encoding.write(1);
             }
         } else {
             // second and subsequent updates
-            let Some(existing_tail_record) = self.table.page_directory.get(&base_indirection_column) else {
+            let Some(existing_tail_record) =
+                self.table.page_directory.get(&base_indirection_column)
+            else {
                 return false;
             };
 
             // update schema encoding of the tail to be 1 (since record has changed)
             let addrs_existing = existing_tail_record.addresses.lock().unwrap();
-            let mut lastest_tail_schema_encoding = addrs_existing[self.table.page_range.base_container.schema_encoding_column as usize].page.lock().unwrap();
+            let mut lastest_tail_schema_encoding = addrs_existing
+                [self.table.page_range.base_container.schema_encoding_column as usize]
+                .page
+                .lock()
+                .unwrap();
             lastest_tail_schema_encoding.write(1);
         }
 
         let new_rid = self.table.num_records;
         let mut values = vec![new_rid, 0, base_indirection_column];
         values.extend(columns.iter().map(|&x| x as u64));
-        self.table.page_range.tail_container.insert_record(new_rid, values);
+        self.table
+            .page_range
+            .tail_container
+            .insert_record(new_rid, values);
 
         // update the indirection column of the base record
-        let mut indirection_base = addrs_base[self.table.page_range.base_container.indirection_column as usize].page.lock().unwrap();
+        let mut indirection_base = addrs_base
+            [self.table.page_range.base_container.indirection_column as usize]
+            .page
+            .lock()
+            .unwrap();
         indirection_base.write(new_rid);
 
         self.table.num_records += 1;
 
         return true;
-}
+    }
 
     fn sum(&mut self, start_primary_key: u64, end_primary_key: u64, col_index: u64) -> i64 {
         self.table
@@ -92,4 +111,41 @@ impl RQuery {
     fn sum_version(&mut self) {}
 
     fn increment(&mut self) {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_insert_and_read_test() {
+        let mut db = RDatabase::new();
+        let t = db.create_table(String::from("Grades"), 3, 0);
+        let mut q = RQuery::new(t);
+
+        q.insert(vec![1, 2, 3]);
+
+        // Use primary_key of 1
+        let vals = q.select(1, 0, vec![1, 1, 1]);
+        assert_eq!(vals.unwrap(), vec![0, 0, 0, 1, 2, 3]);
+    }
+
+    #[test]
+    fn test_update_read_test() {
+        let mut db = RDatabase::new();
+        let t = db.create_table(String::from("Grades"), 3, 0);
+        let mut q = RQuery::new(t);
+
+        q.insert(vec![1, 2, 3]);
+
+        // Use primary_key of 1
+        let vals = q.select(1, 0, vec![1, 1, 1]);
+        assert_eq!(vals.unwrap(), vec![0, 0, 0, 1, 2, 3]);
+
+        let success = q.update(1, vec![1, 5, 6]);
+        assert!(success);
+
+        let vals2 = q.select(1, 0, vec![1, 1, 1]);
+        assert_eq!(vals2.unwrap(), vec![0, 0, 0, 1, 5, 6]);
+    }
 }
