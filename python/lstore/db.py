659f335acab7701b3be4dbd32d3c79b2529fc0b0
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-02-12 17:00:21 -0800

    Fix comments

--- python/lstore/db.py
+++ python/lstore/db.py
@@ -2,7 +2,6 @@ from .lstore import RDatabase, RTable
 
 
 class Database:
-
     def __init__(self):
         self.db = RDatabase()
 
@@ -12,33 +11,25 @@ class Database:
     def close(self):
         self.db.close()
 
-    """
-    # Creates a new table
-    :param name: string         #Table name
-    :param num_columns: int     #Number of Columns: all columns are integer
-    :param key: int             #Index of table key in columns
-    """
-
     def create_table(
         self,
         name: str,
         num_columns: int,
         key_index: int,
     ) -> RTable:
+        """Creates a new table
+        :param name: string         #Table name
+        :param num_columns: int     #Number of Columns: all columns are integer
+        :param key: int             #Index of table key in columns
+        """
         self.db.create_table(name, num_columns, key_index)
 
         return self.db.get_table_from_index(0)
 
-    """
-    # Deletes the specified table
-    """
-
     def drop_table(self, name: str):
+        """Deletes the specified table"""
         self.db.drop_table(name)
 
-    """
-    # Returns table with the passed name
-    """
-
     def get_table(self, name: str) -> RTable:
+        """Returns table with the passed name"""
         return self.db.get_table(name)
