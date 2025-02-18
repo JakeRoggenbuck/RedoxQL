c50ccf3318969eba8685b3e5a805ab2527d1336e
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-02-18 14:03:10 -0800

    Change colors

--- python/benchmarks/graph_scale.py
+++ python/benchmarks/graph_scale.py
@@ -1,16 +1,33 @@
 import matplotlib.pyplot as plt
 
-
 counts = [10, 100, 1000, 10000, 100000, 1000000, 10000000]
 execution_times = [0.0000, 0.0001, 0.0007, 0.0101, 0.0880, 1.1142, 9.8154]
 
 plt.figure(figsize=(10, 6))
-plt.plot(counts, execution_times, marker='o')
+
+plt.style.use('dark_background')
+fig = plt.gcf()
+fig.patch.set_facecolor('#222222')
+ax = plt.gca()
+ax.set_facecolor('#222222')
+
+plt.plot(counts, execution_times, marker='o', color='mediumseagreen',
+         linewidth=2, markersize=8, markeredgecolor='lightgreen',
+         markerfacecolor='mediumseagreen')
+
 plt.xscale('log')
 plt.yscale('log')
-plt.title('Time Taken to Insert Records vs. Number of Records')
-plt.xlabel('Number of Records (log scale)')
-plt.ylabel('Time Taken (seconds, log scale)')
-plt.grid(True, which="both", linestyle='--', linewidth=0.5)
-plt.xticks(counts)
+
+plt.title('Time Taken to Insert Records vs. Number of Records',
+          color='#DDD', pad=20)
+plt.xlabel('Number of Records (log scale)', color='#DDD')
+plt.ylabel('Time Taken (seconds, log scale)', color='#DDD')
+
+plt.grid(True, which="both", linestyle='--', linewidth=0.5,
+         color='#444444', alpha=0.7)
+
+plt.xticks(counts, color='#DDD')
+plt.yticks(color='#DDD')
+
+plt.tight_layout()
 plt.show()
