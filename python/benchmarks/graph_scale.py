6d0b7eba1e821eb8e9c7b4c48492682bc4bd1a4b
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-02-18 14:15:31 -0800

    Add scale for updates

--- python/benchmarks/graph_scale.py
+++ python/benchmarks/graph_scale.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 
 counts = [10, 100, 1000, 10000, 100000, 1000000, 10000000]
-execution_times = [0.0000, 0.0001, 0.0007, 0.0101, 0.0880, 1.1142, 9.8154]
+insert_execution_times = [0.0000, 0.0001, 0.0007, 0.0101, 0.0880, 1.1142, 9.8154]
+update_execution_times = [0.0000, 0.0002, 0.0017, 0.0207, 0.2312, 2.6942, 26.4918]
 
 plt.figure(figsize=(10, 6))
 
@@ -11,7 +12,7 @@ fig.patch.set_facecolor('#222222')
 ax = plt.gca()
 ax.set_facecolor('#222222')
 
-plt.plot(counts, execution_times, marker='o', color='mediumseagreen',
+plt.plot(counts, insert_execution_times, marker='o', color='mediumseagreen',
          linewidth=2, markersize=8, markeredgecolor='lightgreen',
          markerfacecolor='mediumseagreen')
 
@@ -19,6 +20,33 @@ plt.xscale('log')
 plt.yscale('log')
 
 plt.title('Time Taken to Insert Records vs. Number of Records',
+          color='#DDD', pad=20)
+plt.xlabel('Number of Records (log scale)', color='#DDD')
+plt.ylabel('Time Taken (seconds, log scale)', color='#DDD')
+
+plt.grid(True, which="both", linestyle='--', linewidth=0.5,
+         color='#444444', alpha=0.7)
+
+plt.xticks(counts, color='#DDD')
+plt.yticks(color='#DDD')
+
+plt.tight_layout()
+plt.show()
+
+plt.style.use('dark_background')
+fig = plt.gcf()
+fig.patch.set_facecolor('#222222')
+ax = plt.gca()
+ax.set_facecolor('#222222')
+
+plt.plot(counts, update_execution_times, marker='o', color='mediumseagreen',
+         linewidth=2, markersize=8, markeredgecolor='lightgreen',
+         markerfacecolor='mediumseagreen')
+
+plt.xscale('log')
+plt.yscale('log')
+
+plt.title('Time Taken to Update Records vs. Number of Records',
           color='#DDD', pad=20)
 plt.xlabel('Number of Records (log scale)', color='#DDD')
 plt.ylabel('Time Taken (seconds, log scale)', color='#DDD')
--- python/benchmarks/scaling_tester.py
+++ python/benchmarks/scaling_tester.py
@@ -14,6 +14,22 @@ def run_inserts(amount: int):
         query.insert(10 + i, 93, 0, 0, 0)
 
 
+def run_updates(amount: int):
+    db = Database()
+    grades_table = db.create_table('Grades', 5, 0)
+    query = Query(grades_table)
+
+    for i in range(0, amount):
+        query.insert(10 + i, 93, 0, 0, 0)
+
+    for i in range(0, amount):
+        query.update(10 + i, *[10 + i, 100, 10, 20, 30])
+
+
+# for x in counts:
+#     execution_time = timeit.timeit(lambda: run_inserts(x), number=1)
+#     print(f"Time taken to insert {x} records: {execution_time:.4f} seconds")
+
 for x in counts:
-    execution_time = timeit.timeit(lambda: run_inserts(x), number=1)
-    print(f"Time taken to insert {x} records: {execution_time:.4f} seconds")
+    execution_time = timeit.timeit(lambda: run_updates(x), number=1)
+    print(f"Time taken to update {x} records: {execution_time:.4f} seconds")
