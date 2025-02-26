f979d43bb707db17d8095dd900b84d8417570150
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-02-25 23:09:26 -0800

    Save the state of the page

--- src/page.rs
+++ src/page.rs
@@ -1,5 +1,7 @@
 use pyo3::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Write};
 
 static MAX_SIZE_RECORD: i64 = i64::MAX;
 
@@ -35,6 +37,24 @@ impl PhysicalPage {
 
     pub fn read(&self, index: usize) -> Option<i64> {
         Some(self.data[index])
+    }
+
+    pub fn save_state(&self) {
+        let hardcoded_filename = "./page.data";
+
+        let table_bytes: Vec<u8> = bincode::serialize(&self).expect("Should serialize.");
+
+        let mut file = BufWriter::new(File::create(hardcoded_filename).expect("Should open file."));
+        file.write_all(&table_bytes).expect("Should serialize.");
+    }
+
+    pub fn load_state(&self) -> PhysicalPage {
+        let hardcoded_filename = "./page.data";
+
+        let file = BufReader::new(File::open(hardcoded_filename).expect("Should open file."));
+        let page: PhysicalPage = bincode::deserialize_from(file).expect("Should deserialize.");
+
+        return page;
     }
 }
 
