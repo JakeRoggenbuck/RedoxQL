635646d487861c8d345e41e167c8d6f3750deeaf
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-03-08 01:34:35 -0800

    Better local test script

--- local_pr_tester.sh
+++ local_pr_tester.sh
@@ -7,3 +7,6 @@ cargo build --release
 cargo test
 
 maturin build --release
+
+python3 testM2.py > ./test-outputs/M2.out
+cat ./test-outputs/M2.out | grep -b100 --color=always "Wrong" ./test-outputs/M2.out
