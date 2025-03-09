18bed3ce5b71346e7434ac750a622df239a6e3f1
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-03-09 13:03:21 -0700

    Add run function

--- src/transaction_worker.rs
+++ src/transaction_worker.rs
@@ -1,9 +1,10 @@
 use super::transaction::RTransaction;
 use pyo3::prelude::*;
+use std::collections::VecDeque;
 
 #[pyclass]
 pub struct RTransactionWorker {
-    transactions: Vec<RTransaction>,
+    transactions: VecDeque<RTransaction>,
 }
 
 #[pymethods]
@@ -11,16 +12,28 @@ impl RTransactionWorker {
     #[new]
     pub fn new() -> Self {
         RTransactionWorker {
-            transactions: Vec::new(),
+            transactions: VecDeque::new(),
         }
     }
 
     pub fn add_transaction(&mut self, t: RTransaction) {
-        self.transactions.push(t);
+        self.transactions.push_back(t);
     }
 
     pub fn run(&mut self) {
         // TODO: Make a new thread!
+
+        while self.transactions.len() > 0 {
+            let transaction = self.transactions.pop_front();
+
+            // TODO: Make this specifically a new thread for run
+            //
+            // TODO: I am going to need to push running threads into a new vec
+            // so that .join can check to see if all of the threads are done
+            if let Some(mut t) = transaction {
+                t.run();
+            }
+        }
     }
 
     pub fn join(&mut self) {
